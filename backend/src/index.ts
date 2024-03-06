e1fd734f0800b6d86e98ef8ee065966df6ad868b
sunil parmar <sunil04ce@gmail.com>
2024-03-06 21:26:13 +0530

    added MyUserRoutes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,9 +11,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req: Request, res: Response) => {
-  res.json({ message: "Hello!" });
-});
+app.use("/api/my/user", myUserRoute);
+
+// app.get("/test", async (req: Request, res: Response) => {
+//   res.json({ message: "Hello!" });
+// });
 
 app.listen(7000, () => {
   console.log("server started on localhost:7000");
