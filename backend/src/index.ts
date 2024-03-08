d393070c4fc4a2ad0fd51be54931df4f9a941e95
sunil parmar <sunil04ce@gmail.com>
2024-03-08 22:45:48 +0530

    added createCurrentUser controller method

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
   console.log("Connected to Database");
